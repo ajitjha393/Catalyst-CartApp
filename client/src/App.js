48de777e960e51ff80f057d702a52f98edeff4f5
Bishwajit Jha <ajitjha393@gmail.com>
2021-06-19 09:45:53 +0530

    Fetching and transforming product listings

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux'
 import store from './store'
 import './App.css'
+import Listings from './components/listings'
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div className="App">
 				<h1>Catalyst Cart App</h1>
+				<Listings />
 			</div>
 		</Provider>
 	)
---
+++ client/src/utils/helpers.js
@@ -0,0 +1,16 @@
+/** @args listing : Product[]
+ *  @op product grouped by user
+ */
+export function filterListingByUser(listings) {
+	let userSet = new Set()
+	listings.forEach((prod) => userSet.add(prod.userId._id))
+	let filteredListing = []
+
+	for (const id of userSet) {
+		filteredListing.push({
+			[id]: listings.filter((p) => p.userId._id === id),
+		})
+	}
+
+	return filteredListing
+}
