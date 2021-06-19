be0d4e2aef1450abe54b4c609ca8d129f17fa599
Bishwajit Jha <ajitjha393@gmail.com>
2021-06-19 11:47:37 +0530

    A user catalogue comp with name and product count

---
+++ client/src/components/user-catalogue.js
@@ -0,0 +1,49 @@
+import { Card, StyledBody, StyledAction } from 'baseui/card'
+import { Button } from 'baseui/button'
+import classes from './user-catalogue.module.css'
+import { random } from '../utils/random'
+
+const cardStyles = {
+	Root: {
+		style: {
+			width: '80%',
+			margin: '2rem auto',
+			maxWidth: '40rem',
+			boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+		},
+	},
+}
+
+function UserCatalogue({ userId, productCount, name }) {
+	return (
+		<div>
+			<Card overrides={cardStyles}>
+				<div className={classes.Container}>
+					<img
+						src={`https://randomuser.me/api/portraits/men/${random(
+							0,
+							100
+						)}.jpg`}
+						alt="random user"
+					/>
+					<div className={classes.Content}>
+						<div>
+							<span>Name</span> : {name}
+						</div>
+						<div>
+							<span>Products</span> : {productCount}
+						</div>
+
+						<div>
+							Some Lorem ipsum description, dolor sit amet
+							consectetur adipisicing elit. Dolor quia inventore
+							fugit esse, consequat
+						</div>
+					</div>
+				</div>
+			</Card>
+		</div>
+	)
+}
+
+export default UserCatalogue
