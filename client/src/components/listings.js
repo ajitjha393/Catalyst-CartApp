c6d27a6dfceeb2b8d11332c94150e1acd971a148
Bishwajit Jha <ajitjha393@gmail.com>
2021-06-19 11:48:03 +0530

    Memoized fetching and rendered list of catalogues

--- client/src/components/listings.js
+++ client/src/components/listings.js
@@ -1,27 +1,51 @@
 import { useEffect } from 'react'
 import { connect } from 'react-redux'
 import axios from 'axios'
+import UserCatalogue from './user-catalogue'
 
-function Listings({ setListings }) {
+function Listings({ listings, setListings }) {
 	useEffect(() => {
-		axios
-			.get('http://localhost:8080/product')
-			.then(({ data }) => {
-				console.log(data)
-				setListings(data.products)
-			})
-			.catch((err) => console.log(err))
+		if (listings.length === 0) {
+			axios
+				.get('http://localhost:8080/product')
+				.then(({ data }) => {
+					console.log(data)
+					setListings(data.products)
+				})
+				.catch((err) => console.log(err))
+		}
 	}, [])
+
+	let mappedCatalogue = null
+	if (listings) {
+		mappedCatalogue = listings.map((catalogue) => {
+			const [userId] = Object.keys(catalogue)
+			const { firstName, lastName } = catalogue[userId][0]['userId']
+			return (
+				<UserCatalogue
+					key={userId}
+					userId={userId}
+					productCount={catalogue[userId].length}
+					name={firstName + ' ' + lastName}
+				/>
+			)
+		})
+	}
 
 	return (
 		<div>
 			<h1>Product Listings...</h1>
+			{mappedCatalogue}
 		</div>
 	)
 }
+
+const mapState = (state) => ({
+	listings: state.product,
+})
 
 const mapDispatch = (dispatch) => ({
 	setListings: (listing) => dispatch.product.setListings(listing),
 })
 
-export default connect(null, mapDispatch)(Listings)
+export default connect(mapState, mapDispatch)(Listings)
